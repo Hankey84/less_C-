480d5db58f7f956927806308661cf87e3edea1a8
Hankey84 <hankeybanister84@gmail.com>
2023-11-15 19:11:26 +0300

    Add condition if else

---
+++ examample005_conditionifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "рома")
+{
+    Console.WriteLine("Категорически тебя приветствую, дорогой!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
